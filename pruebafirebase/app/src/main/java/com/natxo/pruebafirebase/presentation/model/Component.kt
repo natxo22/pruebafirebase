467ecdf35a2f4636fab25e19858e966960a588a5
natxo22 <153107311+natxo22@users.noreply.github.com>
2025-03-11 21:33:05 +0100

    Delete pruebafirebase directory

--- pruebafirebase/app/src/main/java/com/natxo/pruebafirebase/presentation/model/Component.kt
+++
@@ -1,7 +0,0 @@
-package com.natxo.pruebafirebase.presentation.model
-
-class Component (
-    val name: String? = null,
-    val image: String? = null,
-    
-)
--- pruebafirebase/app/src/main/java/com/natxo/pruebafirebase/presentation/model/Relic.kt
+++
@@ -1,7 +0,0 @@
-package com.natxo.pruebafirebase.presentation.model
-
-class Relic(
-    val name: String? = null,
-    val image: String? = null,
-)
-
